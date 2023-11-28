4269392d20c6a2e32b7963cf0bec356cb93d2481
iceymoss <ice_moss@163.com>
2023-11-28 19:20:19 +0800

    feat:完成视频在线观看统计

--- controllers/barrage.go
+++ controllers/barrage.go
@@ -2,20 +2,57 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"fyoukuApi/models"
 	"github.com/astaxie/beego"
 	"github.com/gorilla/websocket"
 	"net/http"
+	"sync"
 )
 
 type BarrageControllers struct {
 	beego.Controller
 }
 
+type VideoStats struct {
+	views map[int]int
+	mu    sync.Mutex
+}
+
+var videoStats = NewVideoStats()
+
+func NewVideoStats() *VideoStats {
+	return &VideoStats{
+		views: make(map[int]int),
+	}
+}
+
+func (vs *VideoStats) AddView(videoID int) {
+	vs.mu.Lock()
+	defer vs.mu.Unlock()
+	vs.views[videoID]++
+}
+
+func (vs *VideoStats) ReduceView(videoID int) {
+	vs.mu.Lock()
+	defer vs.mu.Unlock()
+	vs.views[videoID]--
+}
+
+func (vs *VideoStats) GetViews(videoID int) int {
+	vs.mu.Lock()
+	defer vs.mu.Unlock()
+	return vs.views[videoID]
+}
+
+// WsData 数据格式
 type WsData struct {
-	CurrentTime int
-	EpisodesId  int
+	CurrentTime int //当前时间
+	EpisodesId  int //视频ID
+}
+
+type Barrage struct {
+	BarrageMsg []models.BarrageData `json:"barrage_msg"`
+	UserOnline int                  `json:"user_online"`
 }
 
 // 设置websocket跨域问题
@@ -29,44 +66,62 @@ var (
 
 // BarrageWs 获取弹幕websocket 核心逻辑：使用ws协议，从视频播放开始获取60s的弹幕内容，60s播放结束后再次请求后60s的弹幕内容，
 // 前端这要在60s循环对比弹幕时间和视频播放时间对应，渲染到屏幕即可。
-// @router /barrage/ws [*]
 func (b *BarrageControllers) BarrageWs() {
 	var (
-		conn     *websocket.Conn
-		err      error
-		data     []byte
-		barrages []models.BarrageData
+		conn    *websocket.Conn
+		err     error
+		data    []byte
+		barrage Barrage
 	)
 
 	//将http转为websocket
 	if conn, err = upgrader.Upgrade(b.Ctx.ResponseWriter, b.Ctx.Request, nil); err != nil {
-		goto ERR
+		conn.Close()
 	}
-	fmt.Println("连接到websocket")
+
+	if _, data, err = conn.ReadMessage(); err != nil {
+		conn.Close()
+	}
+
+	var wsDataInfo WsData
+	json.Unmarshal(data, &wsDataInfo)
+
+	videoStats.AddView(wsDataInfo.EpisodesId)
+	ResBarrageMsg(conn, wsDataInfo, barrage)
+
+	//用户退出视频
+	defer func(Eid int) {
+		videoStats.ReduceView(Eid)
+		conn.Close()
+	}(wsDataInfo.EpisodesId)
 
 	//监听消息
 	for {
 		if _, data, err = conn.ReadMessage(); err != nil {
-			goto ERR
+			conn.Close()
 		}
 		var wsData WsData
-		json.Unmarshal([]byte(data), &wsData)
+		json.Unmarshal(data, &wsData)
+		ResBarrageMsg(conn, wsData, barrage)
+	}
+}
 
-		fmt.Println("data:", wsData)
+func ResBarrageMsg(conn *websocket.Conn, wsData WsData, barrage Barrage) {
+	var err error
 
-		//当前时间开始后的60s
-		endTime := wsData.CurrentTime + 60
-		//获取弹幕数据
-		_, barrages, err = models.BarrageList(wsData.EpisodesId, wsData.CurrentTime, endTime)
-		if err == nil {
-			if err := conn.WriteJSON(barrages); err != nil {
-				goto ERR
-			}
+	//当前时间开始后的60s
+	endTime := wsData.CurrentTime + 60
+
+	//获取弹幕数据
+	_, barrage.BarrageMsg, err = models.BarrageList(wsData.EpisodesId, wsData.CurrentTime, endTime)
+
+	//返回在线人数
+	barrage.UserOnline = videoStats.GetViews(wsData.EpisodesId)
+	if err == nil {
+		if err := conn.WriteJSON(barrage); err != nil {
+			conn.Close()
 		}
 	}
-
-ERR:
-	conn.Close()
 }
 
 func (b *BarrageControllers) Save() {
