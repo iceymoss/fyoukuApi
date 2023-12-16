21036ef6a34d261cfca22d8141dfaed304f61831
iceymoss <ice_moss@163.com>
2023-12-16 18:11:41 +0800

    fix: 调整子任务

--- cmd.go
+++ cmd.go
@@ -5,12 +5,13 @@ import (
 	"fyoukuApi/command"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql" //注意：beego在连接数据库时必须导入
 	"net/http"
 )
 
 func initConfig() {
 	beego.LoadAppConfig("ini", "./conf/app.conf")
-	defaultdb := beego.AppConfig.String("defaultdb")
+	defaultdb := beego.AppConfig.String("defaultdb") //default
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 	orm.RegisterDataBase("default", "mysql", defaultdb, 30, 30)
 }
@@ -24,5 +25,6 @@ func main() {
 		}()
 		_ = http.ListenAndServe(":7161", nil)
 	}()
+	initConfig()
 	command.Execute()
 }
